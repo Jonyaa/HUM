28d4d95b329853f29f6304e6699ce32438bd028a
jonyaa <jonatan2001@gmail.com>
2020-03-22 14:17:54 +0200

    Work on the room class

--- models/classes.py
+++ models/classes.py
@@ -7,20 +7,27 @@ class Room:
         self.creation_time = time.gmtime()
         self.id = id
         self.admin_id = admin_id
-        self.questions = {}
+        self.questions = {} # question_id: Question()
+    
+    def add_question(self, question_id, question, desc, options):
+        # rooms[id].add_question("How much?", "description", ["yes", "no", "maybe"])
+        self.questions[question_id] = Question(question, desc, options)
+    
+    def update_hum(self, question_id, vote): # vote = "1010" 
+        for index, option in enumerate(vote):
+            if option == "1":
+                self.questions[question_id].total_hums[index] += 1
 
 
 class Question:
-    def __init__(self, question, desc,
-        answer_1, answer_2, answer_3 = None, answer_4 = None):
+    def __init__(self, question, desc, options): # options = list
         self.question = question
+        self.status = "pending"
         self.desc = desc
-        self.answer_1 = answer_1
-        self.answer_2 = answer_2
+        self.options = {}
+        for index, option in enumerate(options):
+            self.options[index] = option
+        
+        # self.options[1] = Option number 2
 
-        # Create only if answer 3,4 exist
-        if answer_3:
-            self.answer_3 = answer_3
-            if answer_4:
-                self.answer_4 = answer_4
-        self.total_hums = [0, 0, 0, 0] # [2,14,0,0] Means that answer 1 fot 2 hums, answer 2 got 14 hums
+        self.total_hums = [0, 0, 0, 0] # [2,14,0,0] Means that option 1 fot 2 hums, option 2 got 14 hums
