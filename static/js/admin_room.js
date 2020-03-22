8303b21681b0df9b3ab54ccf458e9cba7aa8c4a0
jonyaa <jonatan2001@gmail.com>
2020-03-22 13:12:17 +0200

    test

--- static/js/admin_room.js
+++ static/js/admin_room.js
@@ -1,6 +1,9 @@
 //const socket = io.connect(window.location.host);
 const user_url = document.getElementById("room_user_url"),
-    admin_url = document.getElementById("room_admin_url");
+    admin_url = document.getElementById("room_admin_url"),
+    section_questions = document.getElementsByClassName("questions")[0];
 
 user_url.innerHTML = location.origin + "/room/" + user_url.innerHTML;
 admin_url.innerHTML = location.href;
+
+document.getElementsByClassName("questions")[0].style.height = window.innerHeight - document.getElementsByClassName("metadata")[0].offsetHeight + "px"
