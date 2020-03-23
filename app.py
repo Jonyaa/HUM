c3e5e7e5b37ca15c8e6d414b187e9761ae0a33c9
jonyaa <jonatan2001@gmail.com>
2020-03-23 18:06:09 +0200

    Continuing to merge

--- app.py
+++ app.py
@@ -9,34 +9,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-# room properties: id, name, admin_r_number, time start, time end, ip_list, current_users_num, current_uniqe_ip_num, {qustions dictionary} 
-rooms = {1234: Room(1234, "test", 6, 5678)} # room id: room object
-admin_rooms_dict = {} # {admin_r_number: r_number}
-ROOM_SKELETON = {
-    "name": None,
-    "admin_r_number": None,
-    "time_start": None,
-    "time_end": None,
-    "ip_list": [],
-    "current_users_num": None,
-    "current_uniqe_ip_num": None,
-    "qustions_dictionary": {}
-}
-    
-
-def delete_room(r_number):
-    rooms[r_number] = ROOM_SKELETON
-    admin_rooms_dict[r_number] = None
 
 rooms = {} # {room id: room object}
 admin_rooms_dict = {} # {admin_r_number: r_number}
-rooms["a"] = Room("a", "a", time.time() + (6 * 3600), "b") #For test purpose only
+rooms["a"] = Room("a", "a", time.time() + (6 * 3600), "b") # For test purpose only
+
 
 @app.route('/')
 def index():
     print(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
     # return render_template('index.html', title="HUM")
-    return render_template('admin_room.html', room=rooms[1234])
+    return render_template('admin_room.html', room=rooms["a"])
 
 
 @app.route('/room/<room_id>')
